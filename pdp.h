3810a4ada4320779ef8a8987d90fe4d70cf899e5
Endeavour <guliamov.rn@phystech.edu>
2022-05-09 20:20:23 +0300

    updating

--- pdp.h
+++ pdp.h
@@ -1,19 +1,19 @@
-typedef unsigned char byte; //8 bit
-typedef unsigned short int word; //16 bit
-typedef word Adress; //16 bit
+typedef unsigned char byte;        // 8 бит
+typedef unsigned short int word;   // 16 бит
+typedef word Adress;               // 16 бит
+
 
 #define MEMSIZE (64*1024)
 #define pc reg[7]
 
 #define n_pr 0
-#define y_xo 1                  //xo= XX Offset (8 bits, -128 to +127)
+#define y_xo 1
 #define y_ss (1<<1)
 #define y_dd (1<<2)
-#define y_sup (1<<3)           // sup=NN Number (6 bits)
+#define y_sup (1<<3)
 
-
-#define odata 0177566			// регистр данных дисплея
-#define ostat 0177564			// регистр данных ввода
+#define odata 0177566
+#define ostat 0177564
 
 
 struct Command {
@@ -25,28 +25,28 @@ struct Command {
 };
 
 struct SSDD {
-    word val;		//значение аргумента
-    word adr;       //адрес аргумента
+    word val;   		//value
+    word adr;                   //adress
 };
 
 
 
 void b_write(Adress adr, byte b);   //
-byte b_read(Adress adr);            // чтение и запись
+byte b_read(Adress adr);            // read write functions
 void w_write(Adress adr, word w);   //
 word w_read(Adress adr);            //
 void test_mem();
 
-void load_file();                 // чтение файла
-void mem_dump(Adress adr, word n); // печать стандартного потока
-void trace(int type, const char * str, ...);   // трассировка
+void load_file();
+void mem_dump(Adress adr, word n);             // standart mem
+void trace(int type, const char * str, ...);   // tracing
 void settings(char * opt);
 void Flags(word w);
 void run();
 void print_reg();
 void trace_reg(int type_reg, const char * str, ...);
 
-void do_halt();    // остановка
+void do_halt();    // stop
 void do_mov();     //
 void do_add();     //
 void do_movb();    //
